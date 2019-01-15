095178389325cc998f451ab6fe2c233c2cf83689
andy reyes <11710reyes@gmail.com>
2019-01-15 18:44:32 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,8 +1,7 @@
 require "pry"
 class Hash
   def keys_of(*args)
-     map {
-       |key, value| args.include?(value) ? key : nil }.compact
+     map {|key, value| args.include?(value) ? key : nil }.compact
   end
 end
 puts keys_of(args)
